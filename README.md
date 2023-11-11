a5c29e82f5cd63ba4daa6f0fb2b5505461990109
Elliot Plant <elliotaplant@gmail.com>
2023-11-10 16:29:51 -0800

    Start mp4 conversion

---
+++ README.md
@@ -0,0 +1,19 @@
+# Janus
+
+An open source, install once and use forever alternative to Loom.
+
+Outputs .WebM or .mp4 files. No cloud, no subscription, no time limits.
+
+## Packaging
+
+You'll need to create an `ffmpeg-binaries` directory with this structure:
+
+```
+ffmpeg-binaries
+├── linux
+│   └── ffmpeg
+├── mac
+│   └── ffmpeg
+└── win
+    └── ffmpeg.exe
+```
---
+++ ffmpeg.js
@@ -0,0 +1,18 @@
+const os = require('os');
+const path = require('path');
+
+function getFFmpegPath() {
+  const platform = os.platform();
+  switch (platform) {
+    case 'win32':
+      return path.join(__dirname, 'ffmpeg-binaries/win/ffmpeg.exe');
+    case 'darwin':
+      return path.join(__dirname, 'ffmpeg-binaries/mac/ffmpeg');
+    case 'linux':
+      return path.join(__dirname, 'ffmpeg-binaries/linux/ffmpeg');
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+}
+
+export const ffmpegPath = getFFmpegPath();
